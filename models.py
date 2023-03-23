25907a22ccc2ee21903b73ef5bfbe50a9842a9d8
toneDrei79 <tone0709@icloud.com>
2023-03-23 20:05:44 +0100

    change MaxPool2d sizes

--- models.py
+++ models.py
@@ -17,8 +17,8 @@ class Net8(nn.Module):
                                       nn.Conv2d(in_channels=n2, out_channels=n3, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.Conv2d(in_channels=n3, out_channels=n3, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.BatchNorm2d(n3),
-                                      nn.MaxPool2d(kernel_size=4, stride=4))
-        self.classifier = nn.Sequential(nn.Linear(in_features=n3*int(size_image/64*size_image/64), out_features=n4), nn.ReLU(),
+                                      nn.MaxPool2d(kernel_size=2, stride=2))
+        self.classifier = nn.Sequential(nn.Linear(in_features=n3*int(size_image/32*size_image/32), out_features=n4), nn.ReLU(),
                                         nn.Dropout(0.5),
                                         nn.Linear(in_features=n4, out_features=n_class))
 
@@ -35,7 +35,7 @@ class Net11(nn.Module):
         super().__init__()
         self.featrues = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=n1, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.Conv2d(in_channels=n1, out_channels=n1, kernel_size=3, stride=1, padding=1), nn.ReLU(),
-                                      nn.MaxPool2d(kernel_size=2, stride=2),
+                                      nn.MaxPool2d(kernel_size=4, stride=4),
                                       nn.Conv2d(in_channels=n1, out_channels=n2, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.Conv2d(in_channels=n2, out_channels=n2, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.BatchNorm2d(n2),
@@ -47,7 +47,7 @@ class Net11(nn.Module):
                                       nn.Conv2d(in_channels=n4, out_channels=n4, kernel_size=3, stride=1, padding=1), nn.ReLU(),
                                       nn.BatchNorm2d(n4),
                                       nn.MaxPool2d(kernel_size=2, stride=2))
-        self.classifier = nn.Sequential(nn.Linear(in_features=n4*int(size_image/16*size_image/16), out_features=n5), nn.ReLU(),
+        self.classifier = nn.Sequential(nn.Linear(in_features=n4*int(size_image/32*size_image/32), out_features=n5), nn.ReLU(),
                                         nn.Dropout(0.5),
                                         nn.Linear(in_features=n5, out_features=n4), nn.ReLU(),
                                         nn.Linear(in_features=n4, out_features=n_class))
